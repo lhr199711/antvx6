85e7ea7240f9fe52e1104ab261ee8d62293f1b70
刘华瑞 <1066521901@qq.com>
2022-05-18 15:28:38 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
+<!--
+ * @Description: 
+ * @Date: 2022-05-18 15:23:44
+ * @LastEditTime: 2022-05-18 15:25:34
+-->
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="app">test</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
